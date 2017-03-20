d7f0b3a49b308718959cded27f3d096fc3315c3b
yokyen <yokyen.nguwi@jcu.edu.au>
2017-03-20 12:17:34 +0800

    Added cp5632 workshops

---
+++ CP5632/workshop3/task2.py
@@ -0,0 +1,14 @@
+def calcAverage(s1, s2, s3, s4, s5):
+    # sum up all the 5 scores
+    # return the average
+    pass
+
+def determineGrade(score):
+    # if ....
+    pass
+
+def main():
+    s1 = int(input("Enter first score:"))
+    print("The grade is: ", determineGrade(s1))
+    sum = calcAverage(s1, s2....)
+    print("The sum is :", sum)
---
+++ CP5632/workshop3/task3.py
@@ -0,0 +1,17 @@
+"""
+1
+2
+Fizz
+4
+Buzz
+..
+
+14
+FizzBuzz
+
+for i in range(51):
+    print(i)
+# % for getting the remainder
+
+
+"""
